490aa3419f4548106a4ee2b4170a93afb9ba2f21
Julien Polo <julien.polo@captive.fr>
2024-01-25 13:15:56 +0100

    ✨ Add applicationLegacy configuration

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,6 +1,7 @@
 import { describe, test, expect } from 'vitest';
 import baseJSON from '../base.json';
 import applicationJSON from '../application.json';
+import applicationLegacyJSON from '../applicationLegacy.json';
 import libraryJSON from '../library.json';
 
 describe('Configuration snapshot', () => {
@@ -9,6 +10,9 @@ describe('Configuration snapshot', () => {
   });
   test('application.json', () => {
     expect(applicationJSON).toMatchSnapshot();
+  });
+  test('applicationLegacy.json', () => {
+    expect(applicationLegacyJSON).toMatchSnapshot();
   });
   test('library.json', () => {
     expect(libraryJSON).toMatchSnapshot();
